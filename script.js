fff2f79caa01544ca07bd6f79ca70fa350b16814
RomikAgh <romik.axajanyan22@gmail.com>
2022-08-17 20:46:12 +0300

    "Login-Register (Header)"

--- script.js
+++
@@ -1,7 +0,0 @@
-let offset = 0
-const sliderLine = document.querySelector('.slider-line')
-
-document.querySelector('.one').addEventListener('click', function(){
-    offset = offset + 1010
-    sliderLine.style.left = -offset + 'px'
-})
